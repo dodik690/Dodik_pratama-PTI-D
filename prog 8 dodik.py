60c1782b8f1e0890bd94b0b498729db9e31f62d4
dodik690 <dikssss204@gmail.com>
2024-12-08 12:05:35 +0800

    Add files via upload

---
+++ prog 8 dodik.py
@@ -0,0 +1,20 @@
+# ARITMATIKA (X : + - ** % //)
+
+a = 24
+b = 40
+
+# penjumlahan
+hasil = a + b
+print ("nilai dari a + b = ", hasil)
+
+# perpangkatan
+hasil = a ** b
+print ("nilai dari a ** b = ", hasil)
+
+# modulus
+hasil = a % b
+print ("nilai dari a mod b = ", hasil)
+
+#floor dividion (pembagian terdekat-kebawah)
+hasil = a // b
+print ("nilai dari a flor b = ", hasil)
