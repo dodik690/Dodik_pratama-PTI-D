80176236c5978ab44c9afa48f1722287ae08898a
dodik690 <dikssss204@gmail.com>
2025-01-08 18:14:30 +0800

    Add files via upload

---
+++ prog10 dodik.py
@@ -0,0 +1,23 @@
+# perbandingan
+
+# >,  <, >=,  <=, ==,  !=, is, isnot
+
+x = 5
+
+hasil = x > 6
+print("x > 6 = ", hasil)
+
+hasil = x < 6
+print("x < 6 = ", hasil)
+
+hasil = x >= 6
+print("x >= 6 = ", hasil)
+
+hasil = x <= 6
+print("x < 6= = ", hasil)
+
+hasil = x == 6
+print("x == 6 = ", hasil)
+
+hasil = x != 6
+print("x != 6 = ", hasil)
